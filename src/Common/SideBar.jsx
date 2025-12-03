4bf138d87b9e3296311ee411300daf24817a834b
Rakibul hasan <mdrakibulhasan12346@gmail.com>
2025-12-03 13:00:28 +0600

    fixed side bar

--- src/Common/SideBar.jsx
+++ src/Common/SideBar.jsx
@@ -50,7 +50,7 @@ const SideBar = ({ sidebarOpen, setSidebarOpen }) => {
             <div
                 className={`bg-[#FAF7F2] transform ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
                     } lg:translate-x-0 transition-transform duration-300 ease-in-out
-                  fixed inset-y-0 left-0 z-40 w-[304px] flex flex-col`}
+                  fixed inset-y-0 left-0 z-40 w-[340px] flex flex-col`}
             >
                 {/* Logo */}
                 <div className="px-6 py-8">
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import SideBar from '../Common/SideBar';
 
 const DashboardLayout = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(true);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
 
     return (
         <div className="flex min-h-screen bg-gray-50">
@@ -11,7 +11,7 @@ const DashboardLayout = () => {
             <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
             {/* Main Content */}
-            <div className="flex-1 lg:ml-[200px] transition-all duration-300">
+            <div className="flex-1 lg:ml-[320px] transition-all duration-300">
                 <main className="p-6 lg:p-8">
                     <Outlet />
                 </main>
